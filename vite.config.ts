6fd477a9941e70a892088ae24be4e4a7338144da
Lucas Theves <lucas.theves@indy.fr>
2025-01-09 20:26:03 +0100

    newer dist

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/au-fil-des-emotions/',
   plugins: [
     vue(),
     vueDevTools(),
